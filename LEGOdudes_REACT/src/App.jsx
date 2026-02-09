9e0426309934cd7d0b7a50d50838dffa7949c2fc
ackarlse <anncharlottkarlsen@ymail.com>
2026-02-09 14:01:10 +0100

    Lagt til ruter og layout

--- LEGOdudes_REACT/src/App.jsx
+++ LEGOdudes_REACT/src/App.jsx
@@ -7,6 +7,8 @@ import Products from './components/Products'
 import Header from './components/Header'
 import Nav from './components/Nav'
 import CategoryTitle from './components/CategoryTitle'
+import Layout from './components/Layout'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   const [isOpen, setIsOpen] = useState(false)
@@ -49,16 +51,24 @@ function App() {
    setTotalSum(total)
   }, [cart]);
 
+  function Page(){
+    return(
+          <main>
+            <CategoryTitle />
+            <Products products={products} setCart={setCart} />
+          </main>
+    )
+  }
+
   return (
-    <div id="container">
-      <Header setIsOpen={setIsOpen} cartQuantity={cartQuantity} />
-      <Nav />
-      <main>
-        <CategoryTitle />
-        <Products products={products} setCart={setCart} />
-      </main>
-      <Cart isOpen={isOpen} cart={cart} setCart={setCart} totalSum={totalSum} />
-    </div>
+    <Layout setIsOpen={setIsOpen} cartQuantity={cartQuantity} isOpen={isOpen} cart={cart} setCart={setCart} totalSum={totalSum}>
+      <Routes>
+        <Route index element={<Page />} />
+        <Route path='city' element={<CategoryTitle title="City" />} />
+        <Route path='ninjago' element={<CategoryTitle title="Ninjago" />} />
+      </Routes>
+    </Layout>
+
   )
 }
 
